9f8dcb477e918c679910a85b525001ce2814ea33
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-08-05 23:29:13 +0600

    update1

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -5,6 +5,38 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Drawer'),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.deepPurple[200],
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Center(
+                  child: Text(
+                    'L O G O ',
+                    style: TextStyle(fontSize: 37),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text(
+                  'Page 1',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> FirstPage()));
+                },
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
